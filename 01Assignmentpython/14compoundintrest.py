f808276e490d630f41087a6ff8ffa855bb471967
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-04 16:18:06 +0530

    compound intrest

---
+++ 01Assignmentpython/14compoundintrest.py
@@ -0,0 +1,9 @@
+#calculate compound interest:
+principal = 10000
+rate = 5
+time = 2
+print(f"principal : {principal}")
+print(f"rate : {rate}")
+print(f"time : {time}")
+compound_intrest = principal(1 + r/10)^2
+print(f"compound_intrest : {compound_intrest}")
