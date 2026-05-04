234298829072ea8ce1ead30a7300f293d7ac0817
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-04 12:03:13 +0530

    added new all files

---
+++ 11average-02.py
@@ -0,0 +1,11 @@
+# Calculative average of 3 numbers :
+num1 = 25
+num2 = 35
+num3 = 45
+print(f"num1:{num1}")
+print(f"num2:{num2}")
+print(f"num3:{num3}")
+total=(num1+num2+num3)
+average=total/3
+print(f"total : {total}")
+print(f"average : {average}")
