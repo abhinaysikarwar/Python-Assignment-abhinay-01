1e45a2908055315a83e9469d4d1bd5a3fd974f82
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-04 10:41:33 +0530

    abhinay

---
+++ 01Assignmentpython/09percentage.py
@@ -0,0 +1,5 @@
+# calculate student percentage :
+obtain_marks = 430
+out_of = 500
+percentage=(obtain_marks / out_of) * 100
+print(F"percentage : {percentage}")
---
+++ 01Assignmentpython/10average.py
@@ -0,0 +1,6 @@
+# Calculate total and average of 4 subjects : -
+subjects 1 = 80
+subjects 2 = 75
+subjects 3 = 90 
+subjects 4 = 85
+total = subjects(1 + 2 + 3 + 4)
