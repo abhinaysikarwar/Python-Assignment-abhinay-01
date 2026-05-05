ecc7cf9fdf6bb73c85696746f6412f7a501165a1
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-05 14:57:55 +0530

    cuboid

---
+++ 01Assignmentpython/28cuboid.py
@@ -0,0 +1,9 @@
+#calculate surface area of a cuboid :
+l=4
+b=3
+h=2
+surface_area=2*((l*b) + (b*h) + (h*l))
+print(f"length : {l}")
+print(f"breath : {b}")
+print(f"height : {h}")
+print(f"surface area of a cuboid : {surface_area}")
