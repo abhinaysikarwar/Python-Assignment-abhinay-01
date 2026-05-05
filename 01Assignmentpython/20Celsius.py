7bec6ecb78305c8dae2ed32f201e9b6487b89eb8
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-05 11:47:49 +0530

    Update 20Celsius.py

--- 01Assignmentpython/20Celsius.py
+++ 01Assignmentpython/20Celsius.py
@@ -1,5 +1,5 @@
 #Convert 50°F to celsius:
-value=50°F
-celsius=5/9(value-32)
-print(f"value : {value}")
-print(f"celsius :{celsius}")
+value=50 #fahrenheit
+celsius=5/9 *(value-32)
+print(f"value : {value}°F")
+print(f"celsius :{celsius}°c")
