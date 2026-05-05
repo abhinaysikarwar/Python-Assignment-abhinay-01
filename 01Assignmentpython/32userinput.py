13660f766e51c2ca5529d6947b4103bfc5384650
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-05 17:53:02 +0530

    userinput

---
+++ 01Assignmentpython/32userinput.py
@@ -0,0 +1,15 @@
+# string operation :
+# If user input is :
+name="Dev"
+age='25'
+city="Jaipur"
+hobby="Cooking"
+print(f"name : {name}")
+print(f"age : {age}")
+print(f"city : {city}")
+print(f"hobby : {hobby}")
+print()
+print(f"""Meet {name}, a {age}-year-old enthusiast from {city}.When not busy with daily
+ tasks,{name} loves spending time {hobby}. Life in {city} keeps {name} energetic
+  and curious every single day. with coding as a passion, the future looks creative 
+  and inspiring for {name} in the {city} city.""")
