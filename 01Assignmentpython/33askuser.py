b15a081699d3af96899700006cf7292e5bce0608
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-05 19:52:54 +0530

    askuser

---
+++ 01Assignmentpython/33askuser.py
@@ -0,0 +1,9 @@
+#Create a python program tha asks the user for the following :
+name=input("enter your name :")
+profession=input("enter your profession :")
+quote=input("enter your favorite quote :")
+experience=input("enter years of experience :")
+print(f"Name : {name}")
+print(f"Profession : {profession}")
+print(f"Experience : {experience} years of experience")
+print(f"Quote : {quote}")
