016967f5dbd157aad75afbda1cf028927710cde1
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-21 20:21:17 +0530

    prime

---
+++ 03Assignmentpython/04prime.py
@@ -0,0 +1,11 @@
+#rite a Python program to check if a number provided by the user is prime or not.
+
+num=int(input("Enter the number :"))
+count=0
+for i in range(1,num-1):
+    if num % i==0:
+            count+=1
+if count == 2:
+        print("Even number")
+else:
+    print("NOt prime number")
