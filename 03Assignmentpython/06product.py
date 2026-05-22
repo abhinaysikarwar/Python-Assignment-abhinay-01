277a0ffc6490e8dbee43272eae7cae092d8d243b
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-22 19:37:53 +0530

    product

---
+++ 03Assignmentpython/06product.py
@@ -0,0 +1,10 @@
+#Write a Python program to calculate the product of numbers between a starting 
+#and ending point provided by the user. 
+
+# start_point=int(input("Enter the number"))
+# end_point=int(input("Enter the number"))
+
+# product=1
+# for i in range(start_point,end_point+1):
+#     product=product*i
+# print("product=",product)
