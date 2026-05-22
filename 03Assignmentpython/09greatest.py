c23c84ab7ad907f6762b6878465b844d0bf2e781
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-22 20:30:29 +0530

    gretest

---
+++ 03Assignmentpython/09greatest.py
@@ -0,0 +1,8 @@
+#Write a Python program to find the greatest character from the string "python". 
+
+# char="pyhton"
+# greatest=char[0]
+# for i in char:
+#     if i > greatest:
+#         greatest=i
+# print(greatest)
