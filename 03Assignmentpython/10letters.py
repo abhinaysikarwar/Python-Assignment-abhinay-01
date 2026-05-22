fcab3ef1a04e86e12310465990de37e0597f2065
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-22 21:02:08 +0530

    lettetrs

---
+++ 03Assignmentpython/10letters.py
@@ -0,0 +1,9 @@
+#Write a Python program to display all letters except 'm' and 'i' from the string 
+#"Dreamer infotech". 
+
+
+# s="Dreamer infotech"
+# for i in s:
+#     if i == "m" or i == "i":
+#         continue
+#     print(i,end="")
