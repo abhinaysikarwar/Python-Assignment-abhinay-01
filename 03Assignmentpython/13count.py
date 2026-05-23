af50c2cb3726225f825d0f6d671362fe34956aac
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-23 17:30:45 +0530

    whether

--- 03Assignmentpython/13count.py
+++ 03Assignmentpython/13count.py
@@ -1,8 +1,8 @@
  # Write a Python program to count the total number of characters in a string
 #entered by the user.
 
-s=input("Enter the character  ")
-total=0
-for i in s:
-    total+=1
-print("total=",total)
+# s=input("Enter the character  ")
+# total=0
+# for i in s:
+#     total+=1
+# print("total=",total)
---
+++ 03Assignmentpython/14whether.py
@@ -0,0 +1,25 @@
+#.Write a Python program to check whether a string entered by the user is a
+#palindrome.
+
+# str=input("enter the char : ")
+# rev=" "
+# i=len(str)-1
+# while i >= 0:
+#     rev=rev+str[i]
+#     i-=1
+# if str ==rev:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+
+# str=input("enter the char : ")
+# rev=" "
+# i=len(str)-1
+# while i >= 0:
+#     rev=rev+str[i]
+#     i-=1
+# if str >=rev:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
