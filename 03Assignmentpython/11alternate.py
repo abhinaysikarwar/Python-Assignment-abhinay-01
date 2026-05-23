267242d769844e3a50a457c22dd45e69b3d1c273
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-23 13:29:47 +0530

    revrse.py

--- 03Assignmentpython/11alternate.py
+++ 03Assignmentpython/11alternate.py
@@ -1,8 +1,8 @@
 #Write a Python program to print alternate characters from a given string
 
 
-ch=input("Enter the characters : ")
-s=len(ch)
-for i in range(0,s,2):
-    print(i)
+# ch=input("Enter the characters : ")
+# s=len(ch)
+# for i in range(0,s,2):
+#     print(i)
     
---
+++ 03Assignmentpython/12reverse.py
@@ -0,0 +1,7 @@
+#Write a Python program to reverse a string entered by the user
+
+# s=input("Enter the charcter: ")
+# rev=""
+# for i in s:
+#     rev=i+rev
+# print(rev)
