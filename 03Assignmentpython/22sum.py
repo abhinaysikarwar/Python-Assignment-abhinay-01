24823727f3ba643fb72af664371ed19110325599
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-24 19:29:13 +0530

    sum

---
+++ 03Assignmentpython/22sum.py
@@ -0,0 +1,20 @@
+#  Given a string text = "python", calculate the sum of the indices of its
+#  characters without using the range() or len() functions.
+
+
+# text="python"
+# name= (text)
+# total=0
+# count=1
+# while name == text:
+#     total=total+name
+#     print(total)
+
+
+# text="python"
+# total=0
+# count=0
+# for i in text:
+#     total=total+count
+#     count=count+1
+# print("sum=",total)
