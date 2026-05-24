6ad1fc7ff4ba596b66684da1805c5021be6b7824
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-24 17:47:38 +0530

    dupletter.py

---
+++ 03Assignmentpython/20dupletter.py
@@ -0,0 +1,10 @@
+# .Write a Python program to find duplicate letters between two strings.
+# Example: In "virat" and "rohit", the duplicate letter is "r".
+
+# str1=input("Enter the word1 :")
+# str2=input("Enter the word2 :")
+# dup = ""
+# for i in str1:
+#     if i in str2:
+#         dup = i
+#         print("duplicate=",dup)
