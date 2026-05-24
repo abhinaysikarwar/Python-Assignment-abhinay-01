85a81eaa80bb2bea0fc17b000f191f475b496483
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-24 16:52:36 +0530

    pairs

---
+++ 03Assignmentpython/18pairs.py
@@ -0,0 +1,8 @@
+# Write a Python program to display all possible pairs of 3.
+#Example: 1:3, 2:3, 3:3 , 2:1 , 2:2 ,2:3 , 3:1 ,3:2 ,3:3
+
+
+# for i in range(1,4):
+#     for j in range(1,6):
+#         i+=1
+#         print(j,end=":")
