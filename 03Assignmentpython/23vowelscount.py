16f3429d6657280a2d4dae4dc581c43518a093d9
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-27 17:13:31 +0530

    vowelscount

--- 03Assignmentpython/23vowelscount.py
+++ 03Assignmentpython/23vowelscount.py
@@ -3,9 +3,16 @@
 #Constraint: Do not use indexing (text[i]) or slicing (text[:]).
 
 
-text = "python programming"
+# text = "python programming"
+# count=0
+# for i in text:
+#     if i in "aeiouAEIOU":
+#         count=count+1
+# print(count)
+
+text= "This is python for Devops and MY Name is Abhinay sikarwar"
 count=0
 for i in text:
     if i in "aeiouAEIOU":
-        count=count+1
+        count+=1
 print(count)
