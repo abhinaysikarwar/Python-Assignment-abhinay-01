240eb5647ba904285e78ab27547018671e934e24
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-27 17:48:01 +0530

    greater char string

---
+++ 03Assignmentpython/25gre_char.py
@@ -0,0 +1,10 @@
+# Given : 01275623
+#Write a Python program to find and print the greatest character in the string.
+
+
+# num="01275623"
+# greatest="0"
+# for i in num:
+#     if i > greatest:
+#         greatest = i
+# print(greatest)
