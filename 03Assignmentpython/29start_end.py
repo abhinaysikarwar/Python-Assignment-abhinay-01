a01fa09bf55b1df44f78b07a0dbd39a12bfad383
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-29 22:20:07 +0530

    29start_end.py

---
+++ 03Assignmentpython/29start_end.py
@@ -0,0 +1,12 @@
+# Take two numbers from the user: start and end. Print a string labeling each
+#number in that range as Odd or Even.
+#Output_format : 3:Odd 4:Even 5:Odd 6:Even 7:Odd 8:Even 9:Odd
+
+
+# start=int(input("Enter the number 1 : "))
+# end=int(input("Enter the number 2 : "))
+# for i in range(start,end):
+#     if i % 2==0:
+#         print(f"{i} : Odd ")
+#     else:
+#         print(f"{i} : Even ")
