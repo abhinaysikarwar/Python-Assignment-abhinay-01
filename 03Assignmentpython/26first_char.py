0d5d8be06e479ff8a1c2f507bb115ee4749512b2
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-05-29 22:00:21 +0530

    alternate

---
+++ 03Assignmentpython/26first_char.py
@@ -0,0 +1,6 @@
+# .Given: text = "knowyourself"
+#Goal: Find and print the first character that repeats
+
+
+text = "knowyourself"
+for i in text:.
---
+++ 03Assignmentpython/27alternate.py
@@ -0,0 +1,12 @@
+#  Give : text=”if you think you can not do, you can not show think wisely”
+#Goal: Print the alternate words
+#Constraint: Do not use space between words more than once 
+
+
+text="if you think you can not do,you can not show think wisely"
+for i in range(0,len(text),2):
+    print(text[i],end="")
+
+text = "python"
+for i in range(0, len(text), 2):
+    print(text[i], end=", ")
